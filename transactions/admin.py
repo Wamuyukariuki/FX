6d9b53533b2f47f7bca9ed57e5d978f61a34d2a4
WamuyuKariuki <jaxonkariuki@gmail.com>
2024-11-21 12:21:08 +0300

    registered TransactionAdmin and UserPreferenceAdmin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from .models import Transaction, UserPreference
 
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'customer', 'input_currency', 'output_currency', 'transaction_date')
+    search_fields = ('customer__username', 'identifier')
+
+@admin.register(UserPreference)
+class UserPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'decimal_precision')
