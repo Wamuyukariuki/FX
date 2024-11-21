8fe52731ef06e032adaa5f8111c62f1a27a98633
WamuyuKariuki <jaxonkariuki@gmail.com>
2024-11-21 11:34:44 +0300

    added a new url path for creating and updating user preferrences i.e currencies and decimal precision

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import TransactionCreateView, TransactionListView, TransactionDetailView, AvailableCurrenciesListView
+from .views import TransactionCreateView, TransactionListView, TransactionDetailView, AvailableCurrenciesListView, \
+    UserPreferenceUpdateView
 
 urlpatterns = [
     path('transactions/', TransactionListView.as_view(), name='transaction-list'),
     path('transactions/<int:id>/', TransactionDetailView.as_view(), name='transaction-detail'),
     path('transactions/create/', TransactionCreateView.as_view(), name='transaction-create'),
     path('currencies/', AvailableCurrenciesListView.as_view(), name='available-currencies'),
+    path('update-preferences/', UserPreferenceUpdateView.as_view(), name='update-preferences'),
 
 ]
