3c3618f110526fbb65e1f21c10ef0a9e1fc76af3
WamuyuKariuki <jaxonkariuki@gmail.com>
2024-11-21 11:28:27 +0300

    added user preference model and relations to transaction model, removed customer id

--- transactions/models.py
+++ transactions/models.py
@@ -1,18 +1,33 @@
 import uuid
-
+from django.contrib.auth.models import User
 from django.db import models
+from decimal import Decimal
 
-# Create your models here.
 class Transaction(models.Model):
-    id = models.AutoField(primary_key=True)
-    identifier = models.UUIDField(max_length=15, unique=True, default=uuid.uuid4, editable=False)
-    customer_id = models.IntegerField()
+    identifier = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transactions", db_index=True)
     input_amount = models.DecimalField(max_digits=15, decimal_places=2)
     input_currency = models.CharField(max_length=3)
     output_amount = models.DecimalField(max_digits=15, decimal_places=2)
     output_currency = models.CharField(max_length=3)
     transaction_date = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        # Ensure input_amount and output_amount respect the user's decimal precision
+        if self.input_amount and self.output_amount:
+            decimal_precision = self.customer.preferences.decimal_precision
+            self.input_amount = self.input_amount.quantize(Decimal(10) ** -decimal_precision)
+            self.output_amount = self.output_amount.quantize(Decimal(10) ** -decimal_precision)
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.customer_id} {self.input_amount} {self.input_currency}"
+        return f"Transaction {self.identifier} by {self.customer.username}"
+
+
+class UserPreference(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="preferences")
+    preferred_currencies = models.JSONField(default=list)  # Store subscribed currencies as a list
+    decimal_precision = models.PositiveSmallIntegerField(default=2)  # Store the precision (e.g., 2 dp or 3 dp)
+
+    def __str__(self):
+        return f"{self.user.username}'s Preferences"
